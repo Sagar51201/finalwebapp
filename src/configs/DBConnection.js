6aab81077f924cc75d66ad2214209631897b154a
Sagar51201 <103173778+Sagar51201@users.noreply.github.com>
2024-05-28 17:40:16 +0530

    Update DBConnection.js

--- src/configs/DBConnection.js
+++ src/configs/DBConnection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 import mysql from "mysql2";
 
 let connection = mysql.createConnection({
-    host: "database-1.c3uwiasuqfca.ap-south-1.rds.amazonaws.com",
+    host: "database-2.c3uwiasuqfca.ap-south-1.rds.amazonaws.com",
     user: "admin",
     password: "123456789",
     database: "mydatabase"
